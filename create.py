b8bf3b64005879a5a3b9c75b724fe9474a81fcf8
ThomasDuple <thomasduple@gmail.com>
2021-02-08 11:03:49 +0100

    load level

--- create.py
+++ create.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 f = open("levels/1.json", "w")
 
@@ -7,6 +8,7 @@ arr = []
 for i in range(10):
     arr.append([])
     for j in range(20):
-        arr[i].append(j)
+        arr[i].append(random.randint(0, 1))
 
+print(arr)
 f.write(json.dumps(arr))
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
 import colors
+import showLevel
 
 WIN = pygame.display.set_mode((1024, 728));
 pygame.display.set_caption("Game Jam 2021");
 
 def main():
     run = True
+    WIN.fill(colors.GREY)
+    showLevel.show(1)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        WIN.fill(colors.GREY)
         pygame.display.update()
     pygame.quit()
 if __name__ == "__main__" :
