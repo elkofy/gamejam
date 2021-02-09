4876a44ec1521bd6154559e40f14b2a05e262e34
ProbablyClem <45786362+ProbablyClem@users.noreply.github.com>
2021-02-09 13:28:20 +0100

    merge

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ import colors
 import globals
 from globals import *
 from enum import Enum
+import objects
 
 class Directions(Enum):
     Up = 0
@@ -12,11 +13,6 @@ class Directions(Enum):
     Right = 3
 
 SCALE = (globals.OBJECT_WIDTH - 2 * int((1 - globals.PLAYER_SCALE)/2 * OBJECT_WIDTH), globals.OBJECT_WIDTH - 2 * int((1 - globals.PLAYER_SCALE)/2 * OBJECT_WIDTH))
-
-player_down = pygame.transform.scale(pygame.image.load('assets/player_down.png'), SCALE)
-player_up = pygame.transform.scale(pygame.image.load('assets/player_up.png'), SCALE)
-player_left = pygame.transform.scale(pygame.image.load('assets/player_left.png'), SCALE)
-player_right = pygame.transform.scale(pygame.image.load('assets/player_right.png'), SCALE)
 
 class Player:
     x = 0 #en unité jeu
@@ -48,7 +44,7 @@ class Player:
     
     def move(self):
         keys=pygame.key.get_pressed()
-        if keys[K_w] or keys[K_UP]:
+        if keys[K_z] or keys[K_UP]:
             if not isWall(self.x, self.y -1):
                 self.y -= 1
                 self.energie -=1
@@ -60,7 +56,7 @@ class Player:
                 self.energie -=1
                 self.img = objects.sprites["pl_d"]
                 globals.changeView(self.x, self.y)
-        if keys[K_a] or keys[K_LEFT]:
+        if keys[K_q] or keys[K_LEFT]:
             if not isWall(self.x -1, self.y):
                 self.x -= 1
                 self.energie -=1
