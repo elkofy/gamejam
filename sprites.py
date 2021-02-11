22308dcd6940361b2463e2c0d19f2bbd43410d35
ThomasDuple <thomasduple@gmail.com>
2021-02-11 02:28:35 +0100

    sprites

--- sprites.py
+++ sprites.py
@@ -4,30 +4,35 @@ import globals
 sl = {}
 
 toLoad = {
-    "spawn" : "start.png",
     "bg_tile" : "decors/sol.png",
+    "spawn" : "start.png",
     "bed" : "bed.png",
-    "f_tomato" : "fruits/tomato.gif",
     "m_tomato" : "mobs/tomato.gif",
-    "f_kiwi" : "fruits/kiwi.gif",
-    "m_kiwi" : "mobs/kiwi.gif"
+    "m_kiwi" : "mobs/kiwi.gif",
+    "light_day" : "decors/light_day.png",
+    "light_night" : "decors/light_night.png",
+    "vine" : "decors/vine.png",
 }
 
 def loadSprite(name, f, w = globals.OBJECT_WIDTH, h = globals.OBJECT_HEIGHT):
     global sprites
-    #print("loading " + f + " : ", end="")
+    
+    if globals.LOGS:
+        print("loading assets/" + f + " : ", end="")
     img = pygame.image.load("assets/" + f)
     img = pygame.transform.scale(img, (w, h))
     img.convert()
     sl[name] = img
-    #print("loaded")
+    
+    if globals.LOGS:
+        print("loaded")
 
-def loadWallSprites():
+def loadWall():
     global wallSprites
     for i in range(29):
         loadSprite("wall_" + str(i), "wall/" + str(i) + ".png")
 
-def loadPlayerSprites():
+def loadPlayer():
     dirr = {
         "up" : "u",
         "left" : "l",
@@ -41,11 +46,22 @@ def loadPlayerSprites():
         for i in range(16):
             loadSprite("pl_" + dirr[d] + "_m_" + str(i), "player/" + d + "/move_" + str(i+1) + ".png", globals.PLAYER_WIDTH, globals.PLAYER_HEIGHT)
 
+def loadFruits():
+    fruits = {
+        "kiwi" : 7,
+        "tomato" : 6
+    }
+
+    for f in fruits:
+        for i in range(fruits[f]):
+            loadSprite("f_" + f + "_" + str(i), "fruits/" + f + "/" + str(i) + ".png")
+
 def load():
     loadSprite("blind", "blind.png", globals.WIDTH, globals.HEIGHT)
 
-    loadWallSprites()
-    loadPlayerSprites()
+    loadWall()
+    loadPlayer()
+    loadFruits()
 
     for spr in toLoad:
         loadSprite(spr, toLoad[spr])
